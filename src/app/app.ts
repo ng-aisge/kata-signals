5a7b72393a8eaa0e2784f6de95093554dad2adad
Gerald Aistleitner <aisge@fedora.fritz.box>
2025-08-24 13:16:18 +0200

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,25 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  // TODO: create a signal holding a number
+  // TODO: create a computed signal 'isEven' depending on first signal
+  // TODO: log changes of the number to console using effect
+
+  // TODO: create a signal holding a string[] with log-messages
+  // TODO: add logs to that array and show the messages in the template
+
+  constructor() {
+  }
+
+  inc() {
+  }
+
+  dec() {
+  }
+}
